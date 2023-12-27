09c83ca04998261189eb301af28bdb6a1ae600ae
João Feck <jl.feck@gmail.com>
2023-12-27 13:31:29 -0300

    init repo

---
+++ docs/intro.md
@@ -0,0 +1,34 @@
+---
+slug: /
+sidebar_position: 1
+---
+
+# Introdução
+
+## Bem-vindo(a) à documentação do Titan CaaS! 🦉
+
+Seja parte da revolução no setor financeiro com o Titan CaaS, sua solução definitiva para aprimoramento operacional na análise de crédito. Se você faz parte de uma instituição financeira que busca eficiência e inovação, você está no lugar certo. **Titan CaaS (Credit as a Service)** foi projetado para otimizar o processo de análise de crédito, permitindo que você foque no que realmente importa: tomar decisões precisas e estratégicas.
+
+:::info Sobre nossa documentação
+
+Nossa documentação está em constante evolução. Sua colaboração é fundamental para proporcionar esclarecimentos mais abrangentes em breve.
+
+:::
+
+## O que é o Titan CaaS
+
+O Titan CaaS é uma solução moderna e flexível que permite se adaptar as necessidades de qualquer produto de crédito, oferecendo a gestão completa da sua operação.
+
+**Input, análise, workflow e muito mais. Tudo para sua operação de crédito de ponta a ponta.**
+
+## Como usar nossa documentação
+
+Explore a documentação para encontrar guias detalhados, exemplos práticos e referências essenciais. Este recurso foi desenvolvido para orientar desde os conceitos fundamentais até estratégias avançadas na utilização do Titan CaaS para otimização da sua análise de crédito.
+
+### Principais funcionalidades
+
+- **Criação de operação:** A funcionalidade de criação de operação permite que você importe novas operações de maneira intuitiva, fornecendo todos os recursos necessários para configurar parâmetros, definir etapas e iniciar processos operacionais de forma eficiente.
+- **Atualização de operação:** Mantenha suas operações sempre atualizadas com as mudanças feitas por quaisquer um dos lados gerenciais. A funcionalidade de atualização de operação permite ajustar e modificar operações existentes, garantindo flexibilidade e adaptabilidade às necessidades em constante evolução do seu cenário operacional.
+- **Vinculo de operação:** Integre operações de forma coesa e eficiente por meio da funcionalidade de vínculo de operação. Estabeleça conexões entre aplicações de crédito, proporcionando uma visão holística e facilitando o gerenciamento conjunto.
+
+Estes recursos visam tornar a experiência de gerenciamento de operações no Titan CaaS mais intuitiva, ágil e adaptável às demandas específicas da sua operação financeira.
---
+++ docusaurus.config.ts
@@ -0,0 +1,108 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: "Titan CaaS",
+  tagline: "Documentação",
+  favicon: "img/favicon.png",
+
+  // Set the production url of your site here
+  url: "https://docs.titan.ceoslab.app",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ceoslab', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'pt-br',
+    locales: ['pt-br'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: "",
+      logo: {
+        alt: "Logo Titan",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentação',
+        }
+      ],
+    },
+    footer: {
+      style: 'dark',
+      // links: [
+      //   {
+      //     title: 'Docs',
+      //     items: [
+      //       {
+      //         label: 'Tutorial',
+      //         to: '/docs/intro',
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: 'Community',
+      //     items: [
+      //       {
+      //         label: 'Stack Overflow',
+      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+      //       },
+      //       {
+      //         label: 'Discord',
+      //         href: 'https://discordapp.com/invite/docusaurus',
+      //       },
+      //       {
+      //         label: 'Twitter',
+      //         href: 'https://twitter.com/docusaurus',
+      //       },
+      //     ],
+      //   },
+      // ],
+      copyright: `Copyright © ${new Date().getFullYear()} Titan CaaS, um produto Céos Lab.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
